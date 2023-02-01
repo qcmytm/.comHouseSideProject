aad84f390d032d0c630886eb9d34a83e6a512c9c
Q <qcmytm@gmail.com>
2023-02-01 20:45:32 +0800

    two commit

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const houseRoute = require("./routes").house;
 const passport = require("passport");
 require("./config/passport")(passport);
 const cors = require("cors");
+const path = require("path");
 const PORT = process.env.PORT || 7070;
 //連結mongoDB
 mongoose
@@ -29,14 +30,24 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.use("/api/user", authRoute);
+//只有登入系統的使用者,才能夠新增或註冊課程,驗證JWT
 app.use(
   "/api/houses",
   passport.authenticate("jwt", { session: false }),
   houseRoute
 );
-//只有登入系統的使用者,才能夠新增或註冊課程,驗證JWT
+
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("後端伺服器聆聽在port" + PORT);
