bad25f163a944fe21a4aa512bdd11c278dc36e0b
qcmytm <qcmytm@gmail.com>
2023-02-18 15:06:01 +0800

    final version 2/18

--- routes/findHouse-route.js
+++ routes/findHouse-route.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
 const House = require("../models").house;
+
+router.use((req, res, next) => {
+  console.log("findHouse route 正在接收一個request....");
+  next();
+});
 //用house名稱尋找house
 router.get("/:name", async (req, res) => {
   let { name } = req.params;
